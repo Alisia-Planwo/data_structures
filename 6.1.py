4992868d3e650621efa19601a12f29ae50356914
Alisia-Planwo <alisia.phini@planwo.com>
2024-11-20 20:22:48 +0530

    Add files via upload
    A program to check if two strings are anagrams of each other.

---
+++ 6.1.py
@@ -0,0 +1,7 @@
+def anagrams(string1,string2):
+    string1=string1.replace(" ","").lower()
+    string2=string2.replace(" ","").lower()
+
+    return sorted(string1)==sorted(string2)
+
+print(anagrams("Silent","Listen"))
